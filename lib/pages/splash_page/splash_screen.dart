2cd3931fa30e09afd56d46afa158ff27b54707b8
Faheem <faheempktl@gmail.com>
2025-10-23 01:58:55 +0530

    Add AppColors class, Montserrat font, and complete splash screen with logo

--- lib/pages/splash_page/splash_screen.dart
+++ lib/pages/splash_page/splash_screen.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:dejurebook/constants/app_colors.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      backgroundColor: AppColors.white,
+      body: Center(
+        child: Container(
+          width: 267,
+          height: 62,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: AppColors.black,
+              width: 1.0,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'deJureBook™',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
+                color: AppColors.black,
+                letterSpacing: 0.5,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
